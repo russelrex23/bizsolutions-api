31b5dbe72676bf6ec1cf934fe33d953dd21308cc
russelrex <russelrex_23@yahoo.com>
2021-05-29 02:16:02 +0800

    Revert changes on set response header

--- server.js
+++ server.js
@@ -31,9 +31,6 @@ app.post("/api/sendEmail", async function (req, res) {
     try {
         const { email, site } = req.body;
         await sendEmail(email, site);
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.status(200).json({message: "Email successfully sent!"});
     }
     catch(err) {
