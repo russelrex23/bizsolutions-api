c6a1dc1d49cea38db13183505bf7897b68dfb93f
russelrex <russelrex_23@yahoo.com>
2021-06-03 00:01:46 +0800

    Update usermodel

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -7,14 +7,14 @@ const mongoose = require('mongoose'),
 
 exports.register = function(req, res) {
     const newUser = new User(req.body);
-    newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+    newUser.password = bcrypt.hashSync(req.body.password, 10);
     newUser.save(function(err, user) {
         if (err) {
             return res.status(400).send({
                 message: err
             });
         } else {
-            user.hash_password = undefined;
+            user.password = undefined;
             return res.json(user);
         }
     });
@@ -24,28 +24,10 @@ exports.sign_in = function(req, res) {
     User.findOne({
         email: req.body.email
     }, function(err, user) {
-        if (err) throw err;
+        // if (err) throw err;
         if (!user || !user.comparePassword(req.body.password)) {
             return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
         }
         return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
     });
-};
-
-exports.loginRequired = function(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-
-        return res.status(401).json({ message: 'Unauthorized user!!' });
-    }
-};
-exports.profile = function(req, res, next) {
-    if (req.user) {
-        res.send(req.user);
-        next();
-    }
-    else {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
 };
